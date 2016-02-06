845d05cdd7ad1407938053c01b8a0f712fee294f
Matt Hoover <matthew.a.hoover@gmail.com>
2016-02-06 16:03:27 -0500

    Add functionality for saving data as pickles.
    To retain class aspects of data, pickle objects for later use.

--- prep.py
+++ prep.py
@@ -2,16 +2,21 @@
 import sys
 import os
 import re
+import pickle
 
 import pandas as pd
 import numpy as np
 
-from strategies import Ego, Alter
+from classes import Ego, Alter
 
 
-def read_input_files(input_files):
-    return [pd.read_csv(file, sep=None, engine='python') for file in
-            input_files]
+def read_input_files(input_files, header=None):
+    if header:
+        return [pd.read_csv(file, sep=None, engine='python',
+                header=0, names=header) for file in input_files]
+    else:
+        return [pd.read_csv(file, sep=None, engine='python') for file in
+                input_files]
 
 
 def rename_variables(data):
@@ -29,6 +34,15 @@ def main(config):
     ego = rename_variables(pd.concat(read_input_files(config.ego_input_files)))
     alt = rename_variables(pd.concat(read_input_files(config.alt_input_files)))
 
+    # read in cd4 data, which is survey-dependent
+    if config.name=='baseline':
+        cd4 = pd.concat(read_input_files(config.cd4_input_files,
+                        header=['Egoid', 'cd4']))
+    elif config.name=='midline':
+        cd4 = pd.concat(read_input_files(config.cd4_input_files,
+                        header=['Egoid', 'cd4']))
+        cd4 = cd4[cd4.cd4>0]
+
     # change uncollected data to missing (np.nan)
     ego = change_to_missing(ego)
     alt = change_to_missing(alt)
@@ -40,11 +54,15 @@ def main(config):
     alt = [Alter(group, id) for id, group in alt.groupby('EgoID') if
            (len(group)>1) & ((id>=300) & (id<=475)) |
            ((id>=500) & (id<=526))]
-    import pdb; pdb.set_trace()
+
+    # save data
+    pickle.dump([ego, alt], open('{}/{}'.format(config._path_to_data,
+                'baseline.pkl'), 'wb'))
 
 
 class Config(object):
     _path_to_data = os.getcwd()
+
 
     @property
     def ego_input_files(self):
@@ -56,20 +74,29 @@ class Config(object):
         return ['{}{}{}'.format(self._path_to_data, self._data_dir, fname) for
                 fname in self._alt_inputs]
 
+    @property
+    def cd4_input_files(self):
+        return ['{}{}{}'.format(self._path_to_data, self._data_dir, fname) for
+                fname in self._cd4_inputs]
 
 class BaselineConfig(Config):
+    name = 'baseline'
     _data_dir = '/data/m0/'
     _ego_inputs = ['ego_pt1.csv', 'ego_pt2.csv']
     _alt_inputs = ['alter_pt1.csv', 'alter_pt2.csv']
+    _cd4_inputs = ['missing_cd4.txt']
 
 
 class MidlineConfig(Config):
+    name = 'midline'
     _data_dir = '/data/m6/'
     _ego_inputs = ['ego_pt1.csv', 'ego_pt2.csv']
     _alt_inputs = ['alter_pt1.csv', 'alter_pt2.csv']
+    _cd4_inputs = ['original_cd4.csv', 'missing_cd4.csv']
 
 
 class EndlineConfig(Config):
+    name = 'endline'
     _data_dir = '/data/m12/'
     _ego_inputs = ['ego_pt1.csv', 'ego_pt2.csv']
     _alt_inputs = ['alter_pt1.csv', 'alter_pt2.csv']
@@ -81,5 +108,4 @@ if __name__ == '__main__':
         'm6': MidlineConfig(),
         'm12': EndlineConfig(),
     }
-    if len(sys.argv) > 1 and sys.argv[1] in data_config:
-        main(data_config[sys.argv[1]])
+    main(data_config[sys.argv[1]])
