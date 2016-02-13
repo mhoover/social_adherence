9d562d606a512d1079b25d6c751c815ecf9000c1
Matt Hoover <matthew.a.hoover@gmail.com>
2016-02-12 22:25:18 -0500

    Create and verify first test.

--- tests/prep_tests.py
+++ tests/prep_tests.py
@@ -1,23 +1,38 @@
 ''' Tests for prep functions '''
 import pytest
 
-from social_adherence.prep import *
+import pandas as pd
+
+from prep import *
 
 
-def test_read_input_files(input_files, header=None):
+df = pd.DataFrame({'Col 1': [1] * 3,
+                   'Col2': [2] * 3,
+                  })
+
+# def test_read_input_files(input_files, header=None):
+#     pass
+
+@pytest.mark.parametrize('test, expected',[
+    (df, ['Col_1', 'Col2'])
+])
+def test_rename_variables(test, expected):
+    result = rename_variables(test)
+
+    assert result.columns.tolist() == expected
 
 
-def test_rename_variables(data):
+# def test_change_to_missing(data):
+#     pass
 
 
-def test_change_to_missing(data):
+# def test_convert_dates(data, variable):
+#     pass
 
 
-def test_convert_dates(data, variable):
+# def test_make_dichotomous(variable, **kwargs):
+#     pass
 
 
-def test_make_dichotomous(variable, **kwargs):
-
-
-def test_disclosure_status(data):
-
+# def test_disclosure_status(data):
+#     pass
